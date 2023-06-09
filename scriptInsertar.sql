a3b8965a3bcae27f15dd2d5b2ca0830c27f9a0ef
Romero Ezequiel <romerorezequiel@gmail.com>
2023-06-08 23:18:22 -0300

    script de insertar registros

---
+++ scriptInsertar.sql
@@ -0,0 +1,28 @@
+use DBApi;
+
+Insert Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values 
+('777777', 'Ezequiel', 'Romero', '1234-1111','eze@gmail.com','Buenos Aires')
+
+INSERT Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values
+('999999','Lucas','Lopez','2222-1111','lucas@gmail.com','Tandil')
+
+INSERT Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values
+('8888888', 'Milagros', 'Reina', '3333-22222', 'mili@gmail.com', 'Cordoba')
+
+INSERT Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values
+('6666666', 'Gabriela', 'Fernandez', '5555-3333', 'gabi@gmail.com', 'Mar del Plata')
+
+Insert Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values 
+('3222217', 'Ezequiel', 'Lopez', '1234-1111','eze1@gmail.com','Buenos Aires')
+
+INSERT Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values
+('999999','Marta', 'Cardozo','3333-6666','marta@hotmail.com','Tigre')
+
+INSERT Usuario(documento, nombres, apellidos, telefono, correo, ciudad) values
+('43434343', 'Silvia', 'Messi', '2222-1111', 'sil@hotmail.com', 'Barcelona')
+
+
+
+
+
+select * from Usuario
